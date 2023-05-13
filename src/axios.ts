1e22b8bba11cc85ece935a8329837b35e6701ad7
luocong <2239351258@qq.com>
2023-05-13 15:32:35 +0800

    feat(all the file): 实现一些功能
    实现axios的基础功能和异常错误处理及其测试demo

---
+++ src/axios.ts
@@ -0,0 +1,39 @@
+import { transformRequest, transformResponse } from './helpers/data'
+import { processHeaders } from './helpers/headers'
+import { buildURL } from './helpers/url'
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
+import xhr from './xhr'
+
+function axios(config: AxiosRequestConfig): AxiosPromise {
+  // TODO
+  processConfig(config)
+  return xhr(config).then(res => {
+    return transformResponseData(res)
+  })
+}
+
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformURL(config)
+  config.headers = transformHeaders(config)
+  config.data = transformRequestData(config)
+}
+// 格式化url
+function transformURL(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildURL(url, params)
+}
+// 序列化data
+function transformRequestData(config: AxiosRequestConfig): any {
+  return transformRequest(config.data)
+}
+// 处理Headers的content-type字段
+function transformHeaders(config: AxiosRequestConfig): any {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
+}
+// 反序列化响应的data
+function transformResponseData(res: AxiosResponse): AxiosResponse {
+  res.data = transformResponse(res.data)
+  return res
+}
+export default axios
