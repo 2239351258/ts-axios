6087ab7197f24efee021f4e011b1135eb2104b27
luocong <2239351258@qq.com>
2023-05-31 17:42:31 +0800

    feat: 新增请求取消
    增加两种取消请求的功能

--- src/core/xhr.ts
+++ src/core/xhr.ts
@@ -3,7 +3,7 @@ import { parseHeaders } from '../helpers/headers'
 import { createError } from '../helpers/error'
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
   return new Promise((resolve, reject) => {
-    const { data = null, url, method = 'get', headers, responseType, timeout } = config
+    const { data = null, url, method = 'get', headers, responseType, timeout, cancelToken } = config
 
     const request = new XMLHttpRequest()
 
@@ -50,6 +50,13 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
         request.setRequestHeader(name, headers[name])
       }
     })
+    // 取消请求
+    if (cancelToken) {
+      cancelToken.promise.then(reason => {
+        request.abort()
+        reject(reason)
+      })
+    }
 
     request.send(data)
     // 争对状态码的处理
